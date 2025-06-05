c15623e8902173890fe231b40463000bcf485eb2
kyle.lamorrell@wasa.gov.tt <kyle.lamorrell@wasa.gov.tt>
2025-06-05 09:21:05 -0400

    first commit

---
+++ src/views/Ecommerce.vue
@@ -0,0 +1,25 @@
+<template>
+  <AdminLayout>
+ 
+  </AdminLayout>
+</template>
+
+<script>
+// this contain side bar and navbar
+import AdminLayout from '../components/layout/AdminLayout.vue' 
+/*
+import EcommerceMetrics from '../components/ecommerce/EcommerceMetrics.vue'
+import MonthlyTarget from '../components/ecommerce/MonthlySale.vue'
+import MonthlySale from '../components/ecommerce/MonthlyTarget.vue'
+import CustomerDemographic from '../components/ecommerce/CustomerDemographic.vue'
+import StatisticsChart from '../components/ecommerce/StatisticsChart.vue'
+import RecentOrders from '../components/ecommerce/RecentOrders.vue'
+*/
+export default {
+  components: {
+    AdminLayout,
+
+  },
+  name: 'Ecommerce',
+}
+</script>
